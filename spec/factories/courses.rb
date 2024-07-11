1a802cdb84dd5f4a23d5c3aca202d256b43f40bf
Bhushan Kalode <bhushan.kalode@loconav.com>
2024-07-11 22:36:02 +0530

    added faker, shoulda matcher, factory bot gems & added factories for tutor and cource

---
+++ spec/factories/courses.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :course do
+    name { Faker::Educator.course_name }
+    description { Faker::Lorem.paragraph }
+    start_date { Faker::Date.between(from: 1.year.ago, to: 1.year.from_now) }
+    end_date { Faker::Date.between(from: start_date, to: 1.year.from_now) }
+  end
+end
