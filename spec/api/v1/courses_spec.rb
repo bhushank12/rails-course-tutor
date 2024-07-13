ce5d2bfa290a8ad7871dbc1166399a559ee4286c
Bhushan Kalode <bhushan.kalode@loconav.com>
2024-07-13 15:26:15 +0530

    Minor refactor and add some specs

--- spec/api/v1/courses_spec.rb
+++ spec/api/v1/courses_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe "Api::V1::Courses", type: :request do
   let(:valid_attributes) do
     {
       course: {
-        name: 'Mathematics',
+        name: Faker::Educator.course_name,
         start_date: Time.current,
-        end_date: Time.current + 5.days,
-        description: 'Complete mathematics course',
+        end_date: Time.current + 1.year,
+        description: Faker::Lorem.paragraph,
         tutors_attributes: [
-          { name: 'Abc Pqr', email: "abc@gmail.com" },
-          { name: 'Mno Xyz', email: "mno@gmail.com" }
+          { name: Faker::Name.name, email: Faker::Internet.email(domain: 'gmail.com') },
+          { name: Faker::Name.name, email: Faker::Internet.email(domain: 'gmail.com') }
         ]
       }
     }
@@ -34,6 +34,22 @@ RSpec.describe "Api::V1::Courses", type: :request do
         post '/api/v1/courses', params: { course: { name: '' } }
 
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns error name can't blank" do
+        post '/api/v1/courses', params: { course: { name: '' } }
+        response_body = JSON.parse(response.body)
+
+       expect(response_body["name"]).to include("can't be blank")
+      end
+    end
+
+    context 'uniqueness validation' do
+      it 'returns error when course created with same name' do
+        new_course = FactoryBot.build(:course, name: course.name)
+
+        expect(new_course.valid?).to eq(false)
+        expect(new_course.errors.full_messages).to include("Name has already been taken")
       end
     end
   end
